69d1d74a84cfb6fb80b404e1f088f5776fca67b2
Douglas Santos <douglas_j.santos@hotmail.com>
2024-12-05 19:21:35 -0300

    adicionando validacao customizada nas respostas

--- src/main/java/com/dscatalog/dscatalog/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/dscatalog/dscatalog/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.dscatalog.dscatalog.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -36,6 +38,23 @@ public class ResourceExceptionHandler {
         err.setMesssage(e.getMessage());
         err.setPath(request.getRequestURI());
 
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+
+        ValidationError err = new ValidationError();
+
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        err.setError("Validation exception");
+        err.setMesssage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for(FieldError f : e.getFieldErrors()) {
+            err.addError(f.getField(),f.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
---
+++ src/main/java/com/dscatalog/dscatalog/exceptions/ValidationError.java
@@ -0,0 +1,17 @@
+package com.dscatalog.dscatalog.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+    private List<FieldMessage> erros = new ArrayList<>();
+
+    public List<FieldMessage> getErros() {
+        return erros;
+    }
+
+    public void addError(String fieldName, String message){
+        erros.add(new FieldMessage(fieldName,message));
+    }
+}
